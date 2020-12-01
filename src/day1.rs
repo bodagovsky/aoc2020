3b3c7a057c88bd673fc406c594b8fda15ea64051
Artur Bodagovsky <abfdogovsky@ozon.ru>
2020-12-01 15:59:18 +0300

    day1, star 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,16 +1,32 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
 
 pub(crate) fn sum_2020() {
-    let input: Vec<i32> = fetch_input();
+    let input = fetch_input();
+    let d: Vec<i32> = find_doubles(2020, input.clone());
+    println!("product of double {}", d[0] * d[1]);
+    let diffs: Vec<i32> = input.iter().map(|x| 2020 - *x).collect();
+    for dif in &diffs {
+        let doub = find_doubles(*dif, input.clone());
+        if !doub.is_empty() && doub[0] + doub[1] + (2020 - *dif) == 2020 {
+            println!("product of triplet {}", doub[0] * doub[1] * (2020 - *dif));
+            break;
+        }
+    }
+}
+
+fn find_doubles(n: i32, input: Vec<i32>) -> Vec<i32> {
+    let mut doubles: Vec<i32> = vec![];
     let mut values: HashSet<i32> = HashSet::new();
     for num in &input {
         if values.contains(num) {
-            println!("{}", num * (2020 - *num))
+            doubles.push(*num);
+            doubles.push(n - *num);
         }
-        values.insert(2020 - *num);
+        values.insert(n - *num);
     }
+    doubles
 }
 
 fn fetch_input() -> Vec<i32> {
